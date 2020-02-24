f37e995af5d8e5aa6eb36475f170438517de3924
zerkshaban <60327014+zerkshaban@users.noreply.github.com>
2020-02-24 06:08:43 +0500

    Clearing the tb data.
    Fixing and clearing the data according to need.

---
+++ cleaning_tb_dataset.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 03:36:24 2020
+
+@author: Zerk Shaban 
+
+Goal State
+  country  year    age sex  cases
+0      AD  1996  15-24   f    1.0
+1      AD  1996  15-24   m    0.0
+2      AD  1996  25-34   f    1.0
+3      AD  1996  25-34   m    0.0
+4      AD  1996  35-44   f    0.0
+
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as pp
+
+# Reading the csv file
+tb_dataset = pd.read_csv('tb.csv', encoding= 'latin-1')
+sex = ['f','m','f','m','f']
+
+# Reading the country AD with year 1996
+required_data = tb_dataset.query('country == "AD" and year==1996')
+
+#  Finding the cases for each group of age
+required_data = required_data.melt(['country', 'year'],['f1524', 'm1524', 'f2534', 'm2534', 'f3544' ],'age', 'cases')
+required_data['sex'] = sex 
+
+#Setting the sequence of the schema
+sex = required_data.pop('sex')
+cases = required_data.pop('cases')
+
+required_data['sex'] = sex
+required_data['cases'] = cases
+
+
+
